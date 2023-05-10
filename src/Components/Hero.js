4e887630f87fed6482ab33f5a2ac901197b6ef0f
YusufIA99 <yullemanden@gmail.com>
2023-05-10 23:45:35 +0200

    Comments til hero sektionen

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,46 +1,54 @@
+//1. Import React and CSS
 import React from "react";
 import "./Hero.css";
 
 function Hero() {
-  const handleClickOpen = () => {
-    fetch('/open')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
+  
+    const handleClickOpen = () => {
+        //4. Fetch data from the backend
+        fetch("/open")
+            .then((response) => {
+                // If the response is not ok, throw an error
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
 
-  const handleClickClose = () => {
-    fetch('/close')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
 
-  return (
-    <section id="hero">
-      <div className="Åben" id="åben-btn" onClick={handleClickOpen}>
-        <img src="public/Åbent.PNG" alt="" />
-        <p>Åben gardinerne</p>
-        <button className="btn">Åben</button>
-      </div>
+    const handleClickClose = () => {
+        //4. Fetch data from the backend
+        fetch("/close")
+            .then((response) => {
+                // If the response is not ok, throw an error
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
 
-      <div className="Lukke" id="lukke-btn" onClick={handleClickClose}>
-        <img src="public/Lukket.PNG" alt="" />
-        <p>Luk gardinerne</p>
-        <button className="btn">Lukke</button>
-      </div>
-    </section>
-  );
+   
+    return (
+        <section id="hero">
+            <div className="Åben" id="åben-btn" onClick={handleClickOpen}>
+                <img src="public/Åbent.PNG" alt="" />
+                <p>Åben gardinerne</p>
+                <button className="btn">Åben</button>
+            </div>
+
+            <div className="Lukke" id="lukke-btn" onClick={handleClickClose}>
+                <img src="public/Lukket.PNG" alt="" />
+                <p>Luk gardinerne</p>
+                <button className="btn">Lukke</button>
+            </div>
+        </section>
+    );
 }
 
 export default Hero;
