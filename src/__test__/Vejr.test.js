85b681d2f9d27c6e40d01d18e65d4715aaa7a2ed
YusufIA99 <yullemanden@gmail.com>
2023-05-21 23:30:55 +0200

    Kommentar

--- src/__test__/Vejr.test.js
+++ src/__test__/Vejr.test.js
@@ -5,14 +5,13 @@ import Vejr from '../Components/Vejr';
 import axios from 'axios';
 
 
-//Test placeholder tekst i Vejr sektionen 
-
 test('TekstIPlaceholder', () => {
     render(<Vejr />);
     const Søgefunktion = screen.getByPlaceholderText('Søg efter by eller sted');
     expect(Søgefunktion).toBeInTheDocument();
   });
 
+//respons 200 = succes 
  test('ApiRespons', async () => {
     render(<Vejr />);
     const API = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=f9de4a24900764d24efc22786a7284db';
